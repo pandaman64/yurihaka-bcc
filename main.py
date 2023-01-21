f72d7564456121a5bcf7a97b4af65b7e4d58d7f0
pandaman64 <kointosudesuyo@infoseek.jp>
2023-01-21 23:11:04 +0900

    Add execve tracing

--- main.py
+++ main.py
@@ -64,10 +64,52 @@ TRACEPOINT_PROBE(sched, sched_process_exit) {
 
     return 0;
 }
+
+// TODO: send arbitrary size
+#define ARG_SIZE 128
+
+struct execve_event_t {
+    u32 pid;
+    u32 task_pid;
+    u32 argv_index;
+    char comm[TASK_COMM_LEN];
+    char argv[ARG_SIZE];
+};
+BPF_PERF_OUTPUT(execve_events);
+
+int syscall__execve(
+    struct pt_regs *ctx,
+    const char __user *filename,
+    const char __user *const __user *__argv,
+    const char __user *const __user *__envp
+) {
+    struct task_struct *task = (struct task_struct *)bpf_get_current_task();
+
+    struct execve_event_t event = {};
+    event.pid = bpf_get_current_pid_tgid() >> 32;
+    event.task_pid = task->tgid;
+    bpf_get_current_comm(&event.comm, sizeof(event.comm));
+
+    for (u32 i = 0; i < 20; i++) {
+        char *argp = NULL;
+        bpf_probe_read_user(&argp, sizeof(argp), &__argv[i]);
+        if (argp == NULL) {
+            break;
+        }
+
+        event.argv_index = i;
+        // これstrlenとか無しで大丈夫?
+        bpf_probe_read_user(event.argv, sizeof(event.argv), argp);
+        execve_events.perf_submit(ctx, &event, sizeof(event));
+    }
+
+    return 0;
+}
 """
 
 
 b = BPF(text=program)
+b.attach_kprobe(event=b.get_syscall_fnname("execve"), fn_name="syscall__execve")
 
 kinds = ["EXEC", "EXIT"]
 fmt = "%-18s %-10s %-10s %-6s %-6s %-6s %-32s %-32s"
@@ -79,6 +121,13 @@ def print_event(_cpu, data, _size):
     signal = event.exit_code & 0b11111111
     print(fmt % (event.ts, event.pid, event.tgid, kinds[event.kind], exit_code, signal, event.comm.decode("utf-8"), event.filename.decode("utf-8")))
 
+
+def print_execve_event(_cpu, data, _size):
+    event = b["execve_events"].event(data)
+    fields = { field: getattr(event, field) for field, _ in event._fields_ }
+    print(fields)
+
 b["events"].open_perf_buffer(print_event)
+b["execve_events"].open_perf_buffer(print_execve_event)
 while True:
     b.perf_buffer_poll()
